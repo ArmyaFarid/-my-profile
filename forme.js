3d5034d45ef0f158829fd795be050b2bd647eeda
Armya Bakouan <armyabakouan@icloud.com>
2022-06-04 22:37:56 +0100

    add new java script feature on my personnal website

--- forme.js
+++ forme.js
@@ -1,24 +1,56 @@
+let yesElement=document.querySelector('.btn-green');
+let noElement=document.querySelector('.btn-red');
+let getCvElement=document.querySelector('.getCv-card');
+let pageOnBlur=document.querySelector('#principal-center');
 let goAhead=document.querySelector('.ahead-fix');
 let element=document.querySelector('#profile');
 
-element.addEventListener("click", function() {
-    goAhead.classList.add("ahead-fix-script");
+function changePage(whereToGo)
+{
+    window.location=whereToGo;
+}
+
+noElement.addEventListener("click", function() {
+    pageOnBlur.style.opacity="1";
+    getCvElement.style.display="none";
+
+});
+
+yesElement.addEventListener("click", function() {
+    changePage("cv/curriculumvitae.pdf");
 
 });
  
 
-(function(){
+(
+function(){
 let scrolly = function(){
     var supportPageOffset = window.pageXOffset !== undefined;
     var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
     return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
 }
 
+
+/*let downscroll=function(){
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      return 1;
+   } else {
+      return 0;
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);}*/
+
 /* 
 L'orsque l'on scroll
     Si le menu sort de l'ecran 
     Alors il deviendra fixe
 */
+
+
 let head= document.querySelector('#profile');
 let goAhead= document.querySelector('.ahead-fix');
 let bande= document.querySelector('.alert');
@@ -26,13 +58,25 @@ let rect= head.getBoundingClientRect();
 let top=rect.top + scrolly();
 let width=rect.width;
 //Les fonctions
+
 let onScroll= function(){
     console.log('En scroll');
-    if( scrolly() > top+400){
+    console.log(scrolly());
+    console.log(top+800);
+    if( scrolly() > top+800){
         goAhead.style.display="block";
+        if(scrolly() > top+1500){
+            console.log("work");
+            pageOnBlur.style.opacity="0.1";
+            getCvElement.style.display="block";            
+        }
+        
     }
-    else{
+    else {
         goAhead.style.display="none";
+        pageOnBlur.style.opacity="1";
+        getCvElement.style.display="none";
+        
     }
 }
 
@@ -45,6 +89,7 @@ let onResize= function(){
 //Les listeners
 window.addEventListener('scroll', onScroll);
 window.addEventListener('resize',onResize);
+
 })();
 
 
---
+++ home.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Here is my student profile">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://kit.fontawesome.com/0fee1c775a.js" crossorigin="anonymous"></script>
+	<title>Farid BAKOUAN</title>
+</head> 
+<body>
+
+	<div id="principal-center">
+		<div class="home-card-white" id="profile">
+			<h1>Hello I'm Farid Ben Armya</h1>
+			<a href="Photo.gif"><img src="image/moi.jpg" alt="It is me" id="home-page-photo"></a>
+			<p>
+				I'm Farid Ben Armya BAKOUAN a Computer Science and MultiMedia student at Université Centrale de Tunis.I love learning new skills specially coding. I want to be a good coder and contribute to invent new solution in the domain of Computer science. 
+			</p>
+			<div class="btn-blue"><a href="mailto:armyabakouan@icloud.com" title="I'm waiting for your mail">Visite Lewagon</a></div>
+		</div>
+		
+		<div class="card-white">
+			<h2>Work experience</h2>
+			<div class="incard-info">
+				<p>
+				 	I'm still a modest student. 
+				 	But I was member and learder in some club.  
+
+				</p>
+			</div>
+		</div>
+		
+		<div class="card-white">
+			<h2>Formations</h2>	 
+			<p>
+				<div class="incard-info">
+				 	<ul>
+
+				 		<li>2021 BAC C (Mathématique) </li>
+				 		<li>2020 Diplôme de Préparation Militaire Secondaire </li>
+				 		<li>2019 Diplôme de Préparation Militaire Elementaire</li>
+				 		<li>2019 Diplôme de Agent Sécurité Incendie et Secourisme du Second Degree</li>
+				 	</ul>
+				</div>
+			 	
+			</p>	
+		</div>
+		
+		<div class="card-white">
+			<h2>Skills</h2>
+			<p>
+			 	<div class="incard-info">
+				 	<ul>
+				 		<li>Good speaker</li>
+				 		<li>Work with Agile method</li>
+				 		<li>Curious</li>
+				 		<li>Reactif and multi-tâche..</li>
+				 		<li>Ponctuel</li>
+				 		<li>Intelligent</li>
+				 	</ul>
+				</div>
+		 	</p>
+		</div>
+		 
+		<div class="card-white">
+			<h2>Technologies</h2>
+			<p>
+				<div class="incard-info">
+					
+					<ul>
+						<h3>Programing langages</h3>
+				 		<li>C</li>
+				 		<li>HTML,CSS</li>
+				 		<li>PHP</li>
+				 		<li>JAVA-SCRIPT</li>
+				 		<li>PYTHON</li>
+				 		<li>SQL</li>
+			 		</ul>
+			 	
+			 		
+				 	<ul>
+				 		<h3>Design</h3>
+				 		<li>Adobe Photoshop</li>
+				 		<li>Adobe Illustrator</li>
+				 		<li>Adobe xs</li>
+				 		<li>Adobe nnnn</li>
+			 		</ul>
+
+			 		<ul>
+						<h3>OS</h3>
+				 		<li>Windows</li>
+				 		<li>Ubuntu</li>
+				 		<li>IOS</li>
+				 		<li>Android</li>
+			 		</ul>
+			 	</div>
+			</p>
+		</div>
+		 
+		<div class="card-white">
+			<h2>Languages</h2>
+			<p>
+			 	<div class="incard-info">
+				 	<ul>
+				 		<li>French</li>
+				 		<li>English</li>
+				 		<li>Mooree</li>
+				 		<li>Japonais</li>
+				 	</ul>
+				</div>
+			</p>	
+		</div>
+		 
+
+		<div class="card-white">
+			<h2>Hobbies</h2>
+			<p>
+				<div class="incard-info">
+				 	<ul>
+				 		<li>Coding, yes coding it's a hobby for me</li>
+				 		<li>Sport like basket-ball, hand-ball</li>
+				 		<li>Reading</li>
+				 	</ul>
+				</div>			 	
+			</p>
+		</div>
+		 
+
+		<div class="card-white">
+			<h2>Mindset</h2>
+			<p>
+				<div class="incard-info">
+					<ul>
+				 		<li>I love success</li>
+				 		<li>Perfect make perfect</li>
+				 		<li>Invest in myself once</li>
+				 	</ul>
+
+				</div>
+			 	
+			</p>
+		</div>
+		 
+	    
+	    <div class="card-white contact-center"> 
+		    <h2>How to join me..</h2>
+			<div class="info-center">
+				<p>
+				 	Avenue de la gare,Tunis<br>Tél.: 00000000<br>Courriel: armyabakouan@icloud.com<br>
+				</p>
+			</div>
+
+			<ul class="social-horizontale">
+			 	<li>
+			 		<a href="#"><i class="fa-brands fa-instagram"></i></a>
+			 	</li>
+			 	<li>
+			 		<a href="#"><i class="fa-brands fa-facebook-square"></i></a>
+			 	</li>
+			 	<li>
+			 		<a href="#"><i class="fa-brands fa-github-square"></i></a>
+			 	</li>
+			</ul>
+		</div>
+
+
+	
+		<div class="ahead-fix">
+				<a href="#profile"><i class="fa-solid fa-angles-up"></i></i></a>
+		</div>
+	</div>
+		
+<script src="forme.js" ></script>
+</body>
+</html>
