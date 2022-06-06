7dfad64e907ffe3a7e0162dd40de76c7ba39a884
ArmyaFarid <96503355+ArmyaFarid@users.noreply.github.com>
2022-06-06 04:20:47 +0100

    Update forme.js

--- forme.js
+++ forme.js
@@ -65,7 +65,7 @@ let onScroll= function(){
     console.log(top+800);
     if( scrolly() > top+800){
         goAhead.style.display="block";
-        if(scrolly() > top+1520){
+        if(scrolly() > top+1545){
             console.log("work");
             pageOnBlur.style.opacity="0.1";
             getCvElement.style.display="block";            
